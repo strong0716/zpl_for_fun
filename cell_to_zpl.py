1f520705112021218131bc2085693d6d3750b626
DESKTOP-FAB7I7O\strong <strong0716@hotmail.com>
2023-09-26 23:19:33 +0800

    測試文字

--- cell_to_zpl.py
+++ cell_to_zpl.py
@@ -18,12 +18,13 @@ def parse_custom_syntax(file_content):
     zpl_commands.append(f"^LL{rows * cell_height}")  # 標籤長度
     zpl_commands.append("^LH0,0")   # 標籤起始位置
 
-    # 根據cell指令添加大格子
+    # 根據cell指令添加大格子和文字
     for line in lines[1:]:
         if "cell:" in line:
-            parts = line.replace("cell:", "").strip().split("-")
-            start = parts[0].split(",")
-            end = parts[1].split(",")
+            parts = line.replace("cell:", "").strip().split("|")
+            coordinates = parts[0].strip().split("-")
+            start = coordinates[0].split(",")
+            end = coordinates[1].split(",")
             start_row = int(start[0]) - 1
             start_col = int(start[1]) - 1
             end_row = int(end[0]) - 1
@@ -37,6 +38,21 @@ def parse_custom_syntax(file_content):
             zpl_commands.append(f"^FO{start_col * cell_width},{(end_row + 1) * cell_height}^GB{(end_col - start_col + 1) * cell_width},3,3^FS")
             # 畫右方邊框
             zpl_commands.append(f"^FO{(end_col + 1) * cell_width},{start_row * cell_height}^GB3,{(end_row - start_row + 1) * cell_height},3^FS")
+
+            # 如果有文字，則添加文字
+            if len(parts) > 1 and "txt:" in parts[1]:
+                text_parts = parts[1].split(" ")
+                text = text_parts[1].strip().strip('"')
+                xShift = 0
+                yShift = 0
+                if "xShift:" in line:
+                    xShift = int(line.split("xShift:")[1].split()[0])
+                if "yShift:" in line:
+                    yShift = int(line.split("yShift:")[1].split()[0])
+                
+                text_x = start_col * cell_width + ((end_col - start_col + 1) * cell_width) // 2 + xShift
+                text_y = start_row * cell_height + ((end_row - start_row + 1) * cell_height) // 2 + yShift
+                zpl_commands.append(f"^FO{text_x},{text_y}^CI28^A0N,50,50^FD{text}^FS")  # ^CI28 是用於支持中文的指令
 
     zpl_commands.append("^XZ")
     
